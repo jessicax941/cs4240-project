6af3a883e6343761fc15cc888d53e78ebd9b6075
Justin-Ong <justinong32@gmail.com>
2021-04-09 16:51:42 +0800

    Add exclamation mark prompt with exit and play again options to ending scenes

---
+++ cs4240-project/Assets/Scripts/EndingPrompt.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingPrompt : MonoBehaviour
+{
+    public GameObject prompt;
+    public float radius;
+
+    private void Start()
+    {
+        prompt.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if ((Camera.main.transform.position - transform.position).magnitude < radius)
+        {
+            prompt.SetActive(true);
+        }
+        else
+        {
+            prompt.SetActive(false);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newRotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position).eulerAngles;
+        newRotation.x = 0;
+        newRotation.z = 0;
+        transform.rotation = Quaternion.Euler(newRotation);
+    }
+
+    public void RestartApp()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
